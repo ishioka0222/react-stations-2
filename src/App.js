f97952ef8ba2e10a22eb9e843096fe295584e6b9
Hiroki Ishioka <ishioka0222@gmail.com>
2022-09-29 21:49:07 +0900

    figmaに合わせてデザインを修正

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
@@ -18,14 +17,29 @@ function App() {
 
   return (
     <>
-      <h1>新着スレッド</h1>
-      <ul>
-        {threads.map((thread) => (
-          <li key={thread.id}>
-            <a href={`/threads/${thread.id}`}>{thread.title}</a>
-          </li>
-        ))}
-      </ul>
+      <nav className="bg-green-400 text-white p-5">
+        <div className="flex justify-between">
+          <div className="text-2xl">
+            <a href="/">掲示板</a>
+          </div>
+          <div className="underline">
+            <a href="/">スレッドをたてる</a>
+          </div>
+        </div>
+      </nav>
+      <div className="container mx-auto w-6/12">
+        <h1 className="text-2xl p-3">新着スレッド</h1>
+        <ul>
+          {threads.map((thread) => (
+            <li
+              key={thread.id}
+              className="bg-white border p-3 rounded border-gray-400"
+            >
+              <a href={`/threads/${thread.id}`}>{thread.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
